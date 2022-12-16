ea554a1dd5aa215fac00145ef6ff54705fe5378e
dev-sanghoon <dev.sanghoon@gmail.com>
2022-12-16 19:55:03 +0900

    page for writing blog

---
+++ src/routes/write/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import axios from 'axios';
+
+    let content = '';
+
+    async function uploadLog() {
+        await axios.post('/api/log', { content });
+        content = '';
+    }
+</script>
+
+<textarea bind:value={content}></textarea>
+<button on:click={uploadLog}>Submit</button>
