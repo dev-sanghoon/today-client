252d8caf9f8ea98173123ef7cf4aa7db7396ad0f
dev-sanghoon <dev.sanghoon@gmail.com>
2022-12-28 18:48:48 +0900

    minor fix on client

--- src/routes/write/+page.svelte
+++ src/routes/write/+page.svelte
@@ -5,13 +5,31 @@
 	let result = '';
 
 	async function uploadLog() {
+		result = '저장 중';
 		const response = await axios.post('/api/blog', { content });
 		if (response.data.success) {
-			result = response.data.message;
+			content = '';
+			result = '성공';
+			return;
 		}
+		result = '실패';
 	}
 </script>
 
-<textarea bind:value={content} />
-<button on:click={uploadLog}>Submit</button>
-{@html result}
+<div>
+	<textarea bind:value={content} />
+</div>
+<div>
+	<button on:click={uploadLog}>Submit</button>
+</div>
+<div>
+	결과: {result}
+</div>
+
+<style>
+	textarea {
+		width: 100%;
+		height: 30em;
+		resize: none;
+	}
+</style>
