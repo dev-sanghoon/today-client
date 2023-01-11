3e9d89b9f0ebd978ac0b8cdf0cfc47ba4ec78920
dev-sanghoon <dev.sanghoon@gmail.com>
2023-01-11 18:26:29 +0900

    create separated login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,11 @@
 <script>
-	import axios from 'axios';
-
 	/** @type {import('./$types').PageData} */
 	export let data;
-
-	let { feeds, currentUser } = data;
-	let id = '';
-	let password = '';
-
-	async function doLogin() {
-		const response = await axios.post('/api/login', { id, password }, { withCredentials: true });
-		if (response.data.success) {
-			currentUser = response.data.user;
-		}
-	}
 </script>
 
-<div hidden={!!currentUser}>
-	<input bind:value={id} />
-	<input type="password" bind:value={password} />
-	<button on:click={doLogin}>Login</button>
-</div>
 <h1>Habits of little punk</h1>
 <ul>
-	{#each feeds as feed}
+	{#each data.feeds as feed}
 		<li>
 			<p>
 				<a href="/article/{feed.article}">{feed.title}</a>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import axios from 'axios';
+	import { goto } from '$app/navigation';
+
+	let id = '';
+	let password = '';
+
+	async function doLogin() {
+		const response = await axios.post('/api/login', { id, password }, { withCredentials: true });
+		if (response.data.success) {
+			goto('/');
+		}
+	}
+</script>
+
+<div>
+	<input bind:value={id} />
+	<input type="password" bind:value={password} />
+	<button on:click={doLogin}>Login</button>
+</div>
