b078bcbd1dcbfaf9ce078a5ed038dc453aba4f44
dev-sanghoon <dev.sanghoon@gmail.com>
2023-01-12 19:49:40 +0900

    add csp config

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,15 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		csp: {
+			directives: {
+				'script-src': ['self']
+			},
+			reportOnly: {
+				'script-src': ['self']
+			}
+		}
 	}
 };
 
